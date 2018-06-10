3b19ce9348e7c745ef27c2e102d160427ae3b064
y6xiaomo <xiamo6512@163.com>
2018-06-10 23:38:21 +0800

    add test2.c

---
+++ day02/test2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int find_num_more_than_half(int *arr,int size)
+{
+    if(arr == NULL)
+    {
+        return 0;
+    }
+    int i = 0;
+    int count = 0;
+    for( ;i< size;i++)
+    {
+        if(arr[i] == arr[i+1])
+        {
+            count++;
+        }
+        if(count >= size/2)
+        {
+            return arr[i];
+        }
+    }
+    return 0;
+}
+int main()
+{
+    int arr[20] = {1,2,3,2,2,2,5,4,2};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    int num = find_num_more_than_half(arr,size);
+    printf("%d\n",num);
+    return 0;
+}
