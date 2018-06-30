6f43fe3f1fc1dda8433a94f8c61291e16374d68c
y6xiaomo <xiamo6512@163.com>
2018-07-01 00:15:48 +0800

    isValidBST.c

--- day29/isValidBST.c
+++ day29/isValidBST.c
@@ -10,9 +10,24 @@
 class Solution {
 public:
     bool isValidBST(TreeNode *root) {
-                
-            
+        return isValidBST(root,-1,100);                          
     }
+    bool isValidBST(TreeNode* root,int min,int max)
+    {
+        if(root == NULL)
+        {
+            return true;
+        }
+        if(root->val <= min || root->val >= max)
+        {
+            return false;
+        }
+        return isValidBST(root->left,min,root->val) && 
+               isValidBST(root->right,root->val,max);
 
-
+    }
  };
+int main()
+{
+    return 0;
+}
