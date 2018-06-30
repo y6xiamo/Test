e992a9b362e40830faad6bfc1ccdd9b27ef94144
y6xiaomo <xiamo6512@163.com>
2018-06-30 23:50:57 +0800

    isValidBST.c

---
+++ day29/isValidBST.c
@@ -0,0 +1,18 @@
+#include<iostream>
+
+ struct TreeNode {
+   int val;
+   TreeNode *left;
+   TreeNode *right;
+   TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ };
+
+class Solution {
+public:
+    bool isValidBST(TreeNode *root) {
+                
+            
+    }
+
+
+ };
