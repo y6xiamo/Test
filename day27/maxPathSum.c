a9d312f90f7283740af7c86272cbbbe1b7752e60
y6xiaomo <xiamo6512@163.com>
2018-07-05 23:18:39 +0800

    maxPathSum.c

--- day27/maxPathSum.c
+++ day27/maxPathSum.c
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 
+//二叉树的结构
  // Definition for binary tree
     struct TreeNode {
         int val;
@@ -11,10 +12,11 @@ using namespace std;
 
 class Solution {
 public:
+    //任意一条路径上的最大值
     int maxPathSum(TreeNode *root) {
 
         int max_path = 0;
-        _maxPathSum(root,max_path);
+        _maxPathSum(root,max_path);//用这个函数完成递归
         return max_path;
     
     }
@@ -24,10 +26,11 @@ public:
         {
             return 0;
         }
-        int left = max(_maxPathSum(root->left,maxPath),0);
-        int right = max(_maxPathSum(root->right,maxPath),0);
-        maxPath = max(maxPath,left + right + root->val);
-        return max(left,right) + root->val;
+        //最大路径之和 = 左子树最大路径 + 根结点 + 右子树最大路径
+        int left = max(_maxPathSum(root->left,maxPath),0);//左子树的最大路径(结点值可能为负，所以要和0比较)
+        int right = max(_maxPathSum(root->right,maxPath),0);//右子树的最大路径
+        maxPath = max(maxPath,left + right + root->val);//将当前最大路径和重新计算后的路径中的较大值赋给最大路径
+        return max(left,right) + root->val;//左右子树路径中较大的 + 根结点
 
     };
 
