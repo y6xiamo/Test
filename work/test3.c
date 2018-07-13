160f25f240d0a05c413aa0d5d59d0105b4d58afd
y6xiaomo <xiamo6512@163.com>
2018-07-13 23:33:11 +0800

    test3.c

--- work/test3.c
+++ work/test3.c
@@ -7,43 +7,51 @@ char find_first_alone_num(char str[],int size)
         return 0;
         //非法输入
     }
-    //printf("%d\n",size);
+    printf("size:%d\n",size);
     int i = 0;
     int cur = 0;//用来表示当前的元素
-    int flag = 0;
-    for( ;i <= size;i++)
+    int flag = 0;//
+    for( ;i < size;i++)
     {
-        int j = 1;
+        int j = i+1;
         cur = str[i];
-        for( ; j < size - i;j++)
+        for( ;j < size - i - 1;j++)
         {
-            if(str[j] == cur)
+            if(str[j] != cur)
             {
                 flag = 1;
                 break;
             }
         }
-          if(flag == 0)
+    }
+    
+        if(flag == 0)
           {
               return cur;
           }
-          flag = 0;
+         else
+         {
+             return 0;
+         }
+
+}
  // printf("cur : %c\n",cur);
-    }
-   // if(flag == 0)
-   // {
+   // }
+   // if(flag == 0):   // {
    //     return cur;
    // }
    // else
    // {
-        return 0;
+    //   return 0;
     //}
-}
+
+
 int main()
 {
     char str[]= "asdfasdfo";
     int size = sizeof(str)/sizeof(str[0]);
-    char ret = find_first_alone_num(str,size-1);
+    printf("%d\n",size);
+    char ret = find_first_alone_num(str,size);
     printf("%c\n",ret);
     return 0;
 }
