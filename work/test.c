4fd8206c32a88fbe160599699b52c5f8594001bb
y6xiaomo <xiamo6512@163.com>
2018-07-17 00:06:39 +0800

    test.c

--- work/test.c
+++ work/test.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 //反转n个字符串
+void swap(char* a,char* b)
+{
+    int tmp;
+    tmp = *a;
+    *a = *b;
+    *b = tmp;
+
+}
 void reverse_string(char* str,int size,int offset)
 {
     if(str == NULL)
@@ -17,12 +25,12 @@ void reverse_string(char* str,int size,int offset)
     //定义两个指针，
     //一个指向字符串的头，一个指向字符串的结尾
     char* start = str;
-    char* end = str + offset;
-    int i =
-    for( ;i < size ; ++i)
+    char* end = str + offset -1;
+    while(start < end)
     {
-        swap(*)
-
+        swap(start,end);
+        start++;
+        end--;
     }
 
 
