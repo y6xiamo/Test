19fb78b534d867d8d025255304c5e6298095c7c4
y6xiaomo <xiamo6512@163.com>
2018-07-21 22:50:43 +0800

    remove_element.c

--- day44/remove_element.c
+++ day44/remove_element.c
@@ -5,7 +5,7 @@ public:
     int removeElement(int A[], int n, int elem) {
         int i = 0;
         int count = 0;
-
+        //count用来计数
         for( ;i < A.length; i++)
         {
             if(A[i] != elem)
@@ -15,7 +15,7 @@ public:
             else
             {
 
-                A[1] = A[i+1];
+                A[i] = A[i+1];
             }
 
         }
