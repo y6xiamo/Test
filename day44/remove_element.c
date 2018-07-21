347a87401d9d2390c85347dad1155e26fffed71a
y6xiaomo <xiamo6512@163.com>
2018-07-21 22:53:47 +0800

    remove_element.c

--- day44/remove_element.c
+++ day44/remove_element.c
@@ -1,15 +1,17 @@
 #include <iostream>
 class Solution {
 public:
-    //移除A中的n个元素
+    //移除A中的n个元素,返回删除后的A的长度
     int removeElement(int A[], int n, int elem) {
         int i = 0;
         int count = 0;
-        //count用来计数
+        //count用来计数,表示删除后A中的元素个数
         for( ;i < A.length; i++)
         {
             if(A[i] != elem)
             {
+                //A中元素不等于要删除的元素
+                //count++
                 count++;
             }
             else
