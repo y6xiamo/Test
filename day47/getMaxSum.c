6a3431cf90d85ff2cec2d3e6d71f072669f6f1c3
y6xiaomo <xiamo6512@163.com>
2018-07-23 11:12:21 +0800

    getMaxSum.cpp

--- day47/getMaxSum.c
+++ day47/getMaxSum.c
@@ -1,4 +1,45 @@
 #include <stdio.h>
+//int getMaxSum(int array[],int len)
+//{
+//    if(array == NULL)
+//    {
+//        return 0;
+//        //非法输入
+//    }
+//    if(len > 3000)
+//    {
+//        return 0;
+//        //非法输入
+//    }
+//    int i = 0;
+//    //用于遍历这个数组，是枚举子序列的起点
+//    int max_sum = 0;//最大连续数列和
+//    for( ;i < len; ++i)
+//    {
+//        int j = i;
+//        //枚举子序列的终点
+//        for( ;j < len;++j)
+//        {
+//            int sum = 0;
+//            //每一个子序列的和
+//            int k = i;
+//            //下面这个循环用于求出每个子序列的和
+//            for( ; k <= j ;++k)
+//            {
+//                sum += array[k];
+//            }
+//            //如果子序列的和大于最大子序列的和
+//            //就用他替换掉最大子序列之和
+//            if(sum > max_sum)
+//            {
+//                max_sum = sum;
+//            }
+//
+//        }
+//    }
+//    return max_sum;
+//
+//}
 int getMaxSum(int array[],int len)
 {
     if(array == NULL)
@@ -9,29 +50,12 @@ int getMaxSum(int array[],int len)
     if(len > 3000)
     {
         return 0;
+        //非法输入
     }
-    int i = 0;
-    int max_sum = 0;
-    for( ;i < len; ++i)
-    {
-        int j = i;
-        for( ;j < len;++j)
-        {
-            int sum = 0;
-            int k = i;
-            for( ; k <= j ;++k)
-            {
-                sum += array[k];
-            }
-            if(sum > max_sum)
-            {
-                max_sum = sum;
-            }
+    int max_sum = 0;//用来保存最大连续数列之和
+    int sum = 0;//用来保存每个数列之和
 
-        }
-    }
-    return max_sum;
-
+    
 }
 int main()
 {
---
+++ day47/getMaxSum.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+class MaxSum {
+public:
+    int getMaxSum(vector<int> A, int n)
+    {
+        vector<int> max_sum(n);   //子问题最优解的数组
+        max_sum[0] = A[0];
+        
+        int i = 1;
+        for( ;i < n;++i)
+         {
+            max_sum[i] = max(A[i],max_sum[i-1]+A[i]);
+         }
+
+         i = 0;
+         int sum = 0;
+         for( ; i < n;++i)
+         {
+             if(sum < max_sum[i])
+             {
+                 sum = max_sum[i];
+             }
+         }
+         return sum;
+    }
+};
+int main()
+{
+    return 0;
+}
