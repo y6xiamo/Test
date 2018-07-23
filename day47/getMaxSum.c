d1f4c73aca938e1e60746e2dd567bcd2127a061f
y6xiaomo <xiamo6512@163.com>
2018-07-23 10:03:17 +0800

    getMaxSum.c

--- day47/getMaxSum.c
+++ day47/getMaxSum.c
@@ -4,31 +4,38 @@ int getMaxSum(int array[],int len)
     if(array == NULL)
     {
         return 0;
+        //非法输入
     }
     if(len > 3000)
     {
         return 0;
     }
     int i = 0;
-    int sum = 0;
-    while(i < len)
+    int max_sum = 0;
+    for( ;i < len; ++i)
     {
-        if(array[i] >= 0)
+        int j = i;
+        for( ;j < len;++j)
         {
-            sum += array[i];
-            ++i;
-        }
-        else
-        {
-            break;
+            int sum = 0;
+            int k = i;
+            for( ; k <= j ;++k)
+            {
+                sum += array[k];
+            }
+            if(sum > max_sum)
+            {
+                max_sum = sum;
+            }
+
         }
     }
-    return sum;
+    return max_sum;
 
 }
 int main()
 {
-    int array[5] = {1,2,3,-6,1};
+    int array[] = {0,-1,0,-4,0,0};
     int len = sizeof(array)/sizeof(array[0]);
     int max_sum = getMaxSum(array,len);
     printf("max_sum is %d\n",max_sum);
