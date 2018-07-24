72bb9a3c190684b528d9fbd252d6539c2f5dd276
y6xiaomo <xiamo6512@163.com>
2018-07-24 11:41:47 +0800

    test2.cpp

--- day48/test2.cpp
+++ day48/test2.cpp
@@ -5,18 +5,26 @@ public:
     //{
     //    x = _x;
     //}
-    void testModifyConst(int x)const
+   // void testModifyConst(int x)const
+   // {
+   //     _x = x;;
+   //     modify_x(x);
+   // }
+   // void modify_x(int x)c    {
+   //     x = _x;
+   // }
+   
+    const int * TestReturn(int num)
     {
-        _x = x;;
-        modify_x(x);
-    }
-    void modify_x(int x)c    {
-        x = _x;
+         num = 20;
+        int *a = &num;
+        return a;
     }
 private:
     int _x;
 };
 int main()
 {
+    int *p = TestReturn(10);
     return 0;
 }
