3ffa1dcaa8a906bbb297a1e1a26312706a258c1b
y6xiaomo <xiamo6512@163.com>
2018-07-24 09:58:34 +0800

    test.c

--- day48/test.c
+++ day48/test.c
@@ -6,19 +6,37 @@
 //    FEMALE,
 //    SECRET
 //};
-void test()
-{
-    static int num = 0;
-    num++;
-    printf("%d\n",num);
-}
+//void test()
+//{
+//    static int num = 0;
+//    num++;
+//    printf("%d\n",num);
+//}
 int main()
 {
-    int i = 0;
-    for( ;i < 10;i++)
-    {
-        test();
-    }
+   // int x = 0;
+   // int y = 10;
+   // if(x > 0)
+   // {
+   //     y = 1;
+   // }
+   // else if(x == 0)
+   // {
+   //     y = 0;
+   // }
+   // else
+   // {
+   //     y = -1;
+   // }
+   // printf("y = %d\n",y);
+   // printf("%d\n",43);
+    int i = 43;
+    printf("%d\n",printf("%d\n",printf("%d\n",i)));
+//    int i = 0;
+//    for( ;i < 10;i++)
+//    {
+//        test();
+//    }
 //    printf("%d\n",MALE);
 //    printf("%d\n",FEMALE);
 //    printf("%d\n",SECRET);
