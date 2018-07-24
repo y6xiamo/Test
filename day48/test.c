d5d8496a8830f7d44b701aa99d84fc57dc5d5aee
y6xiaomo <xiamo6512@163.com>
2018-07-24 11:05:06 +0800

    test.c

--- day48/test.c
+++ day48/test.c
@@ -14,12 +14,21 @@
 //}
 int main()
 {
+    int num = 10;
+    int num2 = 0;
+    const int * p1 = &num;
+    *p1 = 20;
+    printf("num = %d\n",num);
+
+    int * const p2 = &num2;
+    p2 = &num;
+
+    const int * const p3 = &num;
+    *p3 = 12;
+    p3 = &num2;
+
    // int x = 0;
-   // int y = 10;
-   // if(x > 0)
-   // {
-   //     y = 1;
-   // }
+   //}
    // else if(x == 0)
    // {
    //     y = 0;
@@ -30,16 +39,17 @@ int main()
    // }
    // printf("y = %d\n",y);
    // printf("%d\n",43);
-    int i = 43;
-    printf("%d\n",printf("%d\n",printf("%d\n",i)));
+   // int i = 43;
+   // printf("%d\n",printf("%d\n",printf("%d\n",i)));
+    //printf返回打印的字符数
 //    int i = 0;
 //    for( ;i < 10;i++)
 //    {
 //        test();
 //    }
-//    printf("%d\n",MALE);
-//    printf("%d\n",FEMALE);
-//    printf("%d\n",SECRET);
+//    printf("%d\n",MALE);//0
+//    printf("%d\n",FEMALE);//1
+//    printf("%d\n",SECRET);//2
 
 //    const int sz = 10;
 //    int arr[10];
@@ -48,17 +58,19 @@ int main()
   //  const int num = 10;
   //  num = 20;
   //  printf("num = %d\n",num);
-   // printf("%d\n",sizeof(char));
-   // printf("%d\n",sizeof(short));
-   // printf("%d\n",sizeof(int));
-   // printf("%d\n",sizeof(long));
-   // printf("%d\n",sizeof(long long));
-   // printf("%d\n",sizeof(float));
-   // printf("%d\n",sizeof(double));
-   // printf("%d\n",strlen("abcdef"));
-   // printf("%d\n",strlen("c:\test\32\test.c"));
+   // printf("%d\n",sizeof(char));//1
+   // printf("%d\n",sizeof(short));//2
+   // printf("%d\n",sizeof(int));//4
+    //printf("%d\n",sizeof(long));//8
+    //printf("%d\n",sizeof(long long));//8
+    //printf("%d\n",sizeof(float));//4
+    //printf("%d\n",sizeof(double));//8
+   // printf("%d\n",strlen("abcdef"));//6
+   // printf("%d\n",strlen("c:\test\32\test.c"));//13
     return 0;
 
   //  printf("%c\n",070);
+  //  //070表示3个8进制数 = 56,对应字符数为8
   // printf("%d\n",0x030);
+  // //0x00表示3个十六进制数 = 48
 }
