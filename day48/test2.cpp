539808c07060f965300a9705d8a0d04ff1263e38
y6xiaomo <xiamo6512@163.com>
2018-07-24 11:32:57 +0800

    test2.cpp

--- day48/test2.cpp
+++ day48/test2.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
 class A{
 public:
-    void testModifyConst(const int x)
+    //void testModifyConst(const int x)
+    //{
+    //    x = _x;
+    //}
+    void testModifyConst(int x)const
     {
+        _x = x;;
+        modify_x(x);
+    }
+    void modify_x(int x)c    {
         x = _x;
     }
 private:
     int _x;
 };
-int mian()
+int main()
 {
     return 0;
 }
