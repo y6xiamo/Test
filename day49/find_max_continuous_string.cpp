7bdb071bc2331bfde9bd0165a3479a3c409483bf
y6xiaomo <xiamo6512@163.com>
2018-07-26 19:01:45 +0800

    查找最长的连续数字串

---
+++ day49/find_max_continuous_string.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string input;
+    int max = 0;
+    string output;
+    string tempstr;
+    while(cin>>input)
+    {
+        int i = 0;
+        for( ;i < input.size();i++)
+        {
+            if(input[i] > '0' && input[i] < '9')
+            {
+                tempstr += input[i];
+                while(input[i+1] > '0' && input[i+1] < '9')
+                {
+                    cout<<i<<endl;
+                    tempstr += input[i+1];
+                    i++;
+                }
+            }
+        }
+        if(tempstr.size() > max)
+        {
+            max = tempstr.size();
+            output = tempstr;
+        }
+        else if(tempstr.size() == max)
+        {
+            output += tempstr;
+        }
+    }
+    cout<<max<<endl;
+    int j = 0;
+    for( ;j < output.size();j++)
+    {
+        cout<<output[j]<<endl;
+        cout<<j<<endl;
+    }
+
+//    cout<<2<<endl;
+    return 0;
+}
+
