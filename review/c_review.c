7ff04d19c1f5131bbc4b1c97dc7cf544d0f7729f
y6xiaomo <xiamo6512@163.com>
2018-07-27 15:43:05 +0800

    c_review.c

--- review/c_review.c
+++ review/c_review.c
@@ -140,8 +140,67 @@ int Add(int* count)
    ++*count;
     return *count;
 }
+
+int my_strlen(const char* arr)
+{
+    int count = 0;
+    while(*arr++ != '\0')
+    {
+        count++;
+    }
+    return count;
+
+}
 int main()
 {
+    char* arr = "abcdef";
+    int count = my_strlen(arr);
+    printf("count = %d\n",count);
+//    int a[3][4] = {0};
+//
+//    printf("%d\n",sizeof(a));
+//    printf("%d\n",sizeof(a[0][0]));
+//    printf("%d\n",sizeof(a[0]));
+//    printf("%d\n",sizeof(a[0]+1));
+//    printf("%d\n",sizeof(a+1));
+//    printf("%d\n",sizeof(&a[0]+1));
+//    printf("%d\n",sizeof(*a));
+//    printf("%d\n",sizeof(a[3]));
+
+//    char *p = "abcdef";
+
+//    printf("%d\n",sizeof(p));
+//    printf("%d\n",sizeof(p+1));
+//    printf("%d\n",sizeof(*p));
+//    printf("%d\n",sizeof(p[0]));
+//    printf("%d\n",sizeof(&p));
+//    printf("%d\n",sizeof(&p+1));
+//    printf("%d\n",sizeof(&p[0]+1));
+
+//    printf("%d\n",strlen(p));
+//    printf("%d\n",strlen(p+1));
+//      printf("%d\n",strlen(*p));
+//    printf("%d\n",strlen(p[0]));
+//    printf("%d\n",strlen(&p));
+//    printf("%d\n",strlen(&p+1));
+//    printf("%d\n",strlen(&p[0]+1));
+//    char arr[] = "abcdef";
+
+//    printf("%d\n",sizeof(arr));
+//    printf("%d\n",sizeof(arr+0));
+//    printf("%d\n",sizeof(*arr));
+//    printf("%d\n",sizeof(arr[1]));
+//    printf("%d\n",sizeof(&arr));
+//    printf("%d\n",sizeof(&arr+1));
+//    printf("%d\n",sizeof(&arr[0]+1));
+  
+//    printf("%d\n",strlen(arr));
+//    printf("%d\n",strlen(arr+0));
+//    printf("%d\n",strlen(*arr));
+//    printf("%d\n",strlen(arr[1]));
+   // printf("%d\n",strlen(&arr));
+   // printf("%d\n",strlen(&arr+1));
+   // printf("%d\n",strlen(&arr[0]+1));
 //      char arr[] = {'a','b','c','d','e','f'};
 
 //    printf("%d\n",sizeof(arr));
