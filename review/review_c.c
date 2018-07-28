8a117e00201f1f7c54280d0f41e6aaf89aa34e3f
y6xiaomo <xiamo6512@163.com>
2018-07-28 23:32:32 +0800

    review_c.c

--- review/review_c.c
+++ review/review_c.c
@@ -17,15 +17,17 @@ int check_sys()
 }
 int main()
 {
-    int ret = check_sys2();
-    if(ret == 1)
-    {
-        printf("大端\n");
-    }
-    else
-    {
-        printf("小端\n");
-    }
+    char a = 128;
+    printf("%u\n",a);
+ //   int ret = check_sys2();
+ //   if(ret == 1)
+ //   {
+ //       printf("大端\n");
+ //   }
+ //   else
+ //   {
+ //       printf("小端\n");
+ //   }
 
     return 0;
 }
