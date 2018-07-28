0dbda15db800a0b32deead95deb9968d81887807
y6xiaomo <xiamo6512@163.com>
2018-07-28 23:40:25 +0800

    c_review.c

--- review/review_c.c
+++ review/review_c.c
@@ -17,11 +17,14 @@ int check_sys()
 }
 int main()
 {
-    char a = -1;
-    signed char b = -1;
-    unsigned char c = -1;
-
-    printf("a = %d b = %d c = %d\n",a,b,c);
+    int i = -20;
+    unsigned int j = 10;
+    printf("%d\n",i+j);
+//    char a = -1;
+//    signed char b = -1;
+//    unsigned char c = -1;
+//
+//    printf("a = %d b = %d c = %d\n",a,b,c);
  //   int ret = check_sys2();
  //   if(ret == 1)
  //   {
