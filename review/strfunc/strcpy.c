ff690ae4d7f6f8a94f6d07b178a92d57c52d376f
y6xiaomo <xiamo6512@163.com>
2018-07-30 17:16:28 +0800

    strcpy.c

---
+++ review/strfunc/strcpy.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+void my_strcpy(char* dst,const char* src)
+{
+    if(dst == NULL || src == NULL)
+    {
+        return;
+    }
+    while(*dst++ = *src++)
+    {
+        ;
+    }
+ //   while(*src)
+ //   {
+ //       *dst = *src;
+ //       dst++;
+ //       src++;
+ //   }
+ //   *dst = '\0';
+}
+int main()
+{
+    char src[] = "abcdef";
+    char dst[128] ={0};
+    my_strcpy(dst,src);
+    int i = 0;
+    for( ;i <(int)(sizeof(dst)/sizeof(dst[0]));i++)
+    {
+        printf("%c ",dst[i]);
+    }
+
+
+    return 0;
+}
