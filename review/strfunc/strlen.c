fb8efe85d70556e129fd148a216eac59ea9290fd
y6xiaomo <xiamo6512@163.com>
2018-07-30 17:01:56 +0800

    strlen.c

--- review/strfunc/strlen.c
+++ review/strfunc/strlen.c
@@ -1,4 +1,32 @@
 #include <stdio.h>
+//借助指针
+int my_strlen3(char* str)
+{
+    if(str == NULL)
+    {
+        return -1;
+    }
+    if(*str == '\0')
+    {
+        return 0;
+    }
+    char* p_str = str;
+    while(*p_str)
+    {
+        p_str++;
+    }
+    return p_str -str;
+}
+//递归
+int my_strlen2(char* str)
+{
+    if(*str == '\0')
+    {
+        return 0;
+    }
+    return 1 + my_strlen2(str+1);
+}
+//借助计数的方式
 int my_strlen(char* str)
 {
     int count = 0;
@@ -12,7 +40,8 @@ int my_strlen(char* str)
 int main()
 {
     char str[] = "abcdef";
-    int len = my_strlen(str);
+//    int len = my_strlen(str);
+    int len = my_strlen2(str);
     printf("len = %d\n",len);
     return 0;
 }
