ccc8b630ca613d75b8a2a5458e2faca2bd959cf8
y6xiaomo <xiamo6512@163.com>
2018-08-01 00:42:14 +0800

    memory.c

--- review/memory.c
+++ review/memory.c
@@ -1,4 +1,24 @@
 #include <stdio.h>
+struct s5
+{
+    int a:2;
+    int b:5;
+    int c:10;
+    int d:30;
+};
+struct s4
+{
+    char c1;
+//    struct s3 a;
+    double b;
+};
+struct s3
+{
+    double b;
+    char c;
+    int i;
+}s3,*p;
+
 struct s2
 {
     char c1;
@@ -16,5 +36,8 @@ int main()
 {
     printf("%d\n",sizeof(struct s1));
     printf("%d\n",sizeof(struct s2));
+    printf("%d\n",sizeof(struct s3));
+    printf("%d\n",sizeof(struct s4));
+    printf("%d\n",sizeof(struct s5));
     return 0;
 }
