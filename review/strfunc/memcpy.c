10117e6e0c491aebe0c69c355befcec1f700edbb
y6xiaomo <xiamo6512@163.com>
2018-07-31 11:23:28 +0800

    memcpy.c

--- review/strfunc/memcpy.c
+++ review/strfunc/memcpy.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include<assert.h>
+#include<string.h>
+//按字节拷贝,遇到\0不会停止
 void *my_memcpy(void* dest,const void* src,size_t count)
 {
     assert(dest);
@@ -22,6 +24,7 @@ int main()
     char src[] = "abcd1234";
     char dest[10] = {0};
     //my_memcpy(dest,src,sizeof(src)/sizeof(src[0]));
-   printf("%s\n",my_memcpy(dest,src,5));
+    printf("%s\n",memcpy(dest,src,5));
+    printf("%s\n",my_memcpy(dest,src,5));
     return 0;
 }
