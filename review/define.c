1a332a09dea96b89b9c01293e96e1ad027c0c7ed
y6xiaomo <xiamo6512@163.com>
2018-08-19 21:08:53 +0800

    define.c

--- review/define.c
+++ review/define.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
-//#define Max(a,b) a * b == 8 ? a * b : b
-#define Max(a,b) (((a) * (b) == 8) ? ((a) * (b)) :(b))
+#define Max(a,b) a * b == 8 ? a * b : b
+//define Max(a,b) (((a) * (b) == 8) ? ((a) * (b)) :(b))
+
+int max(int a,int b)
+{
+    if(a * b == 8)
+    {
+        return a*b;
+    }
+    else
+    {
+        return b;
+    }
+
+}
 int main()
 {
     printf("%d\n",Max(2,3+1));
+    printf("%d\n",max(2,3+1));
     return 0;    
 }
